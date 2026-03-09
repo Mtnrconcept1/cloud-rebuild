9ee0ee7f04a5b12b51877c7265bda17c4aef80fe
mtnrconcept <149347251+mtnrconcept@users.noreply.github.com>
2026-03-09 16:09:08 +0100

    Update HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -31,7 +31,7 @@ export default function HeroSection() {
         aria-hidden="true"
       />
       <div
-        className="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[350px] h-[350px] md:w-[700px] md:h-[700px] bg-white/60 blur-[30px] rounded-full pointer-events-none will-change-transform"
+        className="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[350px] h-[350px] md:w-[600px] md:h-[600px] bg-white/80 blur-[30px] rounded-full pointer-events-none will-change-transform"
         style={{ transform: "translate(calc(-50% + var(--scroll-y, 0px) * -0.1), calc(-50% + var(--scroll-y, 0px) * 0.2))" }}
       />
       <div className="absolute inset-0 shadow-[inset_0_0_120px_rgba(0,0,0,0.10)] pointer-events-none" />
