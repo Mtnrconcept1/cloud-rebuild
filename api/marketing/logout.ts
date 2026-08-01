a1603d33a001d070ec41b7ee95db4505bacf453f
mtnrconcept <149347251+mtnrconcept@users.noreply.github.com>
2026-08-01 20:58:11 +0200

    fix(marketing): use NodeNext-safe BFF import

--- api/marketing/logout.ts
+++ api/marketing/logout.ts
@@ -2,7 +2,7 @@ import {
   marketingLogoutHandler,
   type MarketingApiRequest,
   type MarketingApiResponse,
-} from "../../server/marketingBff";
+} from "../../server/marketingBff.js";
 
 export default function handler(req: MarketingApiRequest, res: MarketingApiResponse) {
   return marketingLogoutHandler(req, res);
